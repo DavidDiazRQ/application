2824f11a1f69c15fc1272f75c6ab75272666c8ec
sdaviddg <david11_d@hotmail.com>
2021-12-02 11:47:54 -0500

    +configuration projet de base react/typescript/material ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,29 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import Header from './components/Header';
+import { BrowserRouter as Router } from 'react-router-dom';
+import { Routes ,Route } from 'react-router-dom';
+import { Home } from './views/Home';
+import { About } from './views/About';
+import { NavBar } from './components/NavBar';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Header title="React App " />
+
+      <Router>
+        <div>
+          <NavBar />
+           <Routes>
+            <Route path="/Home" element={<Home />} />
+            <Route path="/">
+              {/* <Redirect to="/Home" /> */}
+            </Route>
+            <Route path="/About" element={<About />} />
+           </Routes>
+        </div>
+      </Router>
+
+    </>
   );
 }
-
 export default App;
